02edefde98ea60effb7dfabef8d304c362514ff7
俊彦 広瀬 <hirose.toshihiko@syshd01.onmicrosoft.com>
2025-01-29 19:11:54 +0900

    設計更新

---
+++ readCsv.js
@@ -0,0 +1,18 @@
+const readCsv = (filePath) =>{
+    var csvData = new Array();
+	var data = new XMLHttpRequest();	
+	data.open("GET", filePath, false); //true:非同期,false:同期
+    data.send(null);
+
+	var LF = String.fromCharCode(10); //改行ｺｰﾄﾞ
+	var lines = data.responseText.split(LF);
+	for (var i = 0; i < lines.length;++i) {
+		var cells = lines[i].split(",");
+		if(cells.length != 1) {
+			csvData.push(cells);
+		}
+	}
+	return csvData;
+}
+
+export default readCsv;
