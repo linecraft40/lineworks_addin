dc4b3182f181c736a630ead79bc2e33430ebe821
linecraft40 <94527662+linecraft40@users.noreply.github.com>
2025-03-07 09:07:57 +0900

    update

--- popup.js
+++ popup.js
@@ -1,3 +1,18 @@
 setTimeout(() => {
     alert("Hello, world! (from js)");
 }, 700);
+
+const fileInput = document.getElementById('fileInput');
+const fileContent = document.getElementById('fileContent');
+const txt = document.getElementById('txt');
+
+fileInput.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+        fileContent.textContent = reader.result;
+    };
+    txt.innerText = fileContent.textContent;
+    reader.readAsText(file);
+});
